d351e53af2d1f4801bc4e97231a9dcb322c2f656
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2018-11-05 02:51:21 +0300

    fabric-contract-example decoupled to separate repository

---
+++ chaincode/contract-example/src/main/scala/com/github/apolubelov/fabric/contract/example/Main.scala
@@ -0,0 +1,81 @@
+package com.github.apolubelov.fabric.contract.example
+
+import com.github.apolubelov.fabric.contract._
+import com.github.apolubelov.fabric.contract.annotations.{ContractInit, ContractOperation}
+
+/*
+ * This is just a version of
+ * https://github.com/hyperledger/fabric-samples/blob/release-1.3/chaincode/chaincode_example02/java/src/main/java/org/hyperledger/fabric/example/SimpleChaincode.java
+ * Hyperledger Fabric example, ported to Scala with use of contract-base library.
+ *
+ * @author Alexey Polubelov
+ */
+object Main extends ContractBase with App {
+    // start SHIM chain code
+    start(args)
+
+    @ContractInit
+    def init(context: ContractContext, ac1: String, ac1Value: Int, ac2: String, ac2Value: Int): Unit = {
+        context.store.put(ac1, ac1Value)
+        context.store.put(ac2, ac2Value)
+        // bonus content:
+        putAsset(context, "a1", DummyAsset("a1", 1, 1.1))
+        putAsset(context, "a2", DummyAsset("a2", 2, 2.2))
+        putAsset(context, "a3", DummyAsset("a3", 3, 3.3))
+    }
+
+    @ContractOperation
+    def invoke(context: ContractContext, from: String, to: String, x: Int): ContractResponse =
+        context.store.get[Int](from).map { source =>
+            context.store.get[Int](from).map { dest =>
+                context.store.put(from, source - x)
+                context.store.put(to, dest + x)
+                Success()
+            } getOrElse Error(s"No value of $to in the Ledger")
+        } getOrElse Error(s"No value of $from in the Ledger")
+
+
+    @ContractOperation
+    def delete(context: ContractContext, key: String): Unit = {
+        context.store.del[Int](key)
+    }
+
+    @ContractOperation
+    def query(context: ContractContext, key: String): ContractResponse =
+        context.store.get[Int](key).map { value =>
+            Success(value)
+        } getOrElse Error(s"""{"Error":"Nil amount for "$key"}""")
+
+    //
+    // Bonus content
+    //
+
+    case class DummyAsset(
+        name: String,
+        aType: Int,
+        value: Double
+    )
+
+    override def resolveClassByName(name: String): Option[Class[_]] = Some(classOf[DummyAsset])
+
+    @ContractOperation
+    def putAsset(context: ContractContext, address: String, asset: DummyAsset): ContractResponse = {
+        context.store.put(address, asset)
+        Success()
+    }
+
+    @ContractOperation
+    def getAsset(context: ContractContext, address: String): ContractResponse = {
+        val asset: Option[DummyAsset] = context.store.get[DummyAsset](address)
+        asset.map { asset => Success(asset) } getOrElse Error(s"No Asset with address $address")
+    }
+
+    @ContractOperation
+    def listAssets(context: ContractContext): ContractResponse = {
+        val assets: Array[DummyAsset] = context.store.list[DummyAsset]
+          .map(_._2) // take only values
+          .toArray // use Array, as GSON knows nothing about scala collections
+        Success(assets)
+    }
+
+}
